626a74de810be32a1368efb917f7a86b275803dd
Robert Medina <robertmedib1@gmail.com>
2020-05-07 16:16:35 -0500

    List items on the search page

--- app.rb
+++ app.rb
@@ -1,11 +1,20 @@
-require "sinatra"
-require "sinatra/reloader"
-require "lazyrecord"
-require "http"
+# frozen_string_literal: true
+
+require 'sinatra'
+require 'sinatra/reloader'
+require 'lazyrecord'
+require 'http'
 
 helpers do
 end
 
-get "/" do
-  erb :index, layout: :layout
+get '/' do
+  erb :index
+end
+
+get '/search' do
+  erb '/books/book_card'.to_sym
+end
+get '/list-books' do
+  erb '/books/list_books'.to_sym
 end
