0160a7714ca0d1200a2f152023015b2c306d7e3e
alexandell <alexandell@utec.edu.pe>
2020-05-07 18:09:22 -0500

    add index

--- app.rb
+++ app.rb
@@ -6,6 +6,13 @@ require "http"
 helpers do
 end
 
-get "/" do
-  erb :index, layout: :layout
+set :static_cache_control, [:public, max_age: 1]
+
+get "/:page?" do
+  cache_control :public, max_age: 1
+  if params["page"].nil?
+    erb :index, layout: :layout
+  else
+    erb params["page"].to_sym, layout: :layout
+  end
 end
